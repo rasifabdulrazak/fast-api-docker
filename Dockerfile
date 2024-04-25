d6d1862300559f5e9fb302e57501fb5cf53b562f
RasifMohammed <rasif@metrictreelabs.com>
2024-04-25 21:25:06 +0530

    docker initialized

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,26 @@
 FROM python:3.9
 
+# Set environment varibles
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
 COPY ./requirements.txt /tmp/requirements.txt
 
 COPY ./app /app
 
 WORKDIR /app
 
-RUN apt-get update
-RUN pip install --upgrade pip
-RUN pip install -r /tmp/requirements.txt
+RUN apt-get update \
+    && pip install --upgrade pip \
+    && pip install -r /tmp/requirements.txt \
+    && adduser --disabled-password --no-create-home django-user \
+    && mkdir -p /vol/web/static \
+    && mkdir -p /vol/web/media \
+    && chown -R django-user:django-user /vol \
+    && chown -R django-user:django-user /app \
+    && chmod -R 755 /vol \
+    && chmod -R 755 /app 
+
+COPY ./app/static /vol/web/
+
+
--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request,Depends
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.staticfiles import StaticFiles
 
+
+# ======Static files config===========
+STATIC_ROOT = "/vol/web/static/"
+STATIC_URL = "/pub/static/"
+# ====================================
+
+# ======App config====================
 app = FastAPI()
+app.mount(STATIC_URL, StaticFiles(directory=STATIC_ROOT), name="static")
+# ====================================
 
+
+@app.get("/docs", include_in_schema=False)
+async def swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json",
+        swagger_favicon_url=f"{STATIC_URL}images/poonga.jpeg"
+    )
 
 @app.get("/") 
-def read_root():
-    return {"Hello": "World"} 
+def read_root(request:Request):
+    return {"Hello": f"{STATIC_URL}images/poonga.jpeg"} 
